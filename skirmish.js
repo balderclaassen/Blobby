76cc411aa615dcbdac82f5808dd9be453fcaa207
Balder Claassen <balderclaassen@live.nl>
2017-08-12 21:48:09 +0200

    Initial commit, made an on-click (test) event

---
+++ skirmish.js
@@ -0,0 +1,13 @@
+
+function SelectBlob1()
+{
+    var Blobs = document.getElementById("BlobBlue1");
+    Blobs.style.backgroundColor = "#000000";
+}
+
+//var Blobs = document.querySelectorAll(".Blob");
+//Blobs.style.backgroundColor = "#D93600";
+
+
+//document.getElementsByClassName('Blob').setAttribute("class", "style1");
+//document.body.getElementsByTagName('div');
