38f1c23f6716343374364068271b23c7c50bc5aa
Balder Claassen <balderclaassen@live.nl>
2017-08-12 21:55:55 +0200

    added comment about the test and removed other, not working, methods in the comments.

--- skirmish.js
+++ skirmish.js
@@ -1,13 +1,7 @@
 
 function SelectBlob1()
 {
+    //test for onclick 'selected' animation
     var Blobs = document.getElementById("BlobBlue1");
     Blobs.style.backgroundColor = "#000000";
 }
-
-//var Blobs = document.querySelectorAll(".Blob");
-//Blobs.style.backgroundColor = "#D93600";
-
-
-//document.getElementsByClassName('Blob').setAttribute("class", "style1");
-//document.body.getElementsByTagName('div');
