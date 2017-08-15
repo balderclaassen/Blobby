610e6c42282517d86bb3e881494c3ae00cbe690c
Balder Claassen <balderclaassen@live.nl>
2017-08-15 21:33:23 +0200

    Added a (test) SpawnBlob function, spawned at set location, by button, for now. Added a Selected state for blobs, added a message about not being able to Use IE9 or below due to a function I started using.

--- skirmish.js
+++ skirmish.js
@@ -1,5 +1,22 @@
 
-function SelectBlob(div)
+function SelectBlobP1(div)
 {
-    div.style.backgroundColor = "#FFFF00";
+    div.classList.add("SelectedP1");
+    div.style.border = "1px solid #000000";
+}
+
+function SelectBlobP2(div)
+{
+    div.classList.add("SelectedP2");
+    div.style.border = "1px solid #000000";
+}
+
+
+// function MoveBlobP1() {
+//
+// }
+
+function SpawnBlob()
+{
+    BlobBlue2.classList.add("Blob", "BlobBlue");
 }
