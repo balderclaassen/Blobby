5f12294c6bce15680c2bda2dc1b8ebe03abaf547
Balder Claassen <balderclaassen@gmail.com>
2017-08-26 02:05:42 +0200

    Made a function that creates a new blob every time it is called.

--- skirmish.js
+++ skirmish.js
@@ -1,3 +1,14 @@
+// count +1 on every run for Blob ID's.
+var n = 2;
+function incrementBlue()
+{
+    return ++n;
+}
+function incrementRed()
+{
+    return ++n;
+}
+
 
 function SelectBlobP1(div)
 {
@@ -16,7 +27,36 @@ function SelectBlobP2(div)
 //
 // }
 
+//deprecated
 function SpawnBlob()
 {
-    BlobBlue2.classList.add("Blob", "BlobBlue");
+    BlueBlob2.classList.add("Blob", "BlueBlob", "SmallBlob");
+}
+
+
+function CreateRedBlob()
+{
+    var newdiv = document.createElement("div");
+    var container = document.getElementById('RedBlobs');
+    var newdivID = "RedBlob" + incrementRed();
+    container.appendChild(newdiv);
+    newdiv.classList.add("Blob", "SmallBlob", "RedBlob");
+    newdiv.setAttribute("id", newdivID);
+    newdiv.setAttribute("onclick", "SelectBlobP1(this)");
+    //for debugging
+    console.log(newdivID);
+}
+
+function CreateBlueBlob()
+{
+    var newdiv = document.createElement("div");
+    var container = document.getElementById('BlueBlobs');
+    var newdivID = "BlueBlob" + incrementBlue();
+    container.appendChild(newdiv);
+    newdiv.classList.add("Blob", "SmallBlob", "BlueBlob");
+    newdiv.setAttribute("id", newdivID);
+    newdiv.setAttribute("onclick", "SelectBlobP2(this)");
+    //for debugging
+    console.log(newdivID);
+
 }
