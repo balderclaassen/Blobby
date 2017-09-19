dc1c6997ffd5252fed860c61984b5790d3247365
Balder Claassen <balderclaassen@gmail.com>
2017-09-20 01:08:07 +0200

    Moved border styling on selection from JS to CSS
    The CSS for this was done in this commit:
    b1761fd099be353feffc9dafb222fcdfae10da5f

--- skirmish.js
+++ skirmish.js
@@ -18,13 +18,11 @@ function incrementRed()
 function SelectBlobP1(div)
 {
     div.classList.add("SelectedP1");
-    div.style.border = "1px solid #000000";
 }
 
 function SelectBlobP2(div)
 {
     div.classList.add("SelectedP2");
-    div.style.border = "1px solid #000000";
 }
 // |||||||||||||||||||||||||||||||||| Select Blobs end
 
